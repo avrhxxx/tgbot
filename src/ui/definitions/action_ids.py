53ce38ef945fb36693f1591f076b30a9e38811df
avrhxxx <stelczyk0797@gmail.com>
2026-04-22 11:19:37 +0200

    Create action_ids.py

---
+++ src/ui/definitions/action_ids.py
@@ -0,0 +1,16 @@
+# src/ui/definitions/action_ids.py
+
+class ActionID:
+    """
+    All UI actions (buttons / callbacks / events).
+    """
+
+    OPEN_HOME = "open_home"
+
+    OPEN_EVENTS = "open_events"
+    OPEN_EVENT = "open_event"
+
+    NEXT_STEP = "next_step"
+    BACK = "back"
+
+    ADMIN_PANEL = "admin_panel"
