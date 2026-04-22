35b35587fee62c5532489d45ade0005a16a46655
avrhxxx <stelczyk0797@gmail.com>
2026-04-22 11:19:01 +0200

    Create screen_ids.py

---
+++ src/ui/definitions/screen_ids.py
@@ -0,0 +1,19 @@
+# src/ui/definitions/screen_ids.py
+
+class ScreenID:
+    """
+    Central registry for all UI screens.
+    """
+
+    # HOME SYSTEM
+    HOME_R3 = "home_r3"
+    HOME_R4 = "home_r4"
+    HOME_R5 = "home_r5"
+
+    # EVENTS (future)
+    EVENT_LIST = "event_list"
+    EVENT_VIEW = "event_view"
+
+    # WIZARD (future)
+    WIZARD_STEP_1 = "wizard_step_1"
+    WIZARD_STEP_2 = "wizard_step_2"
