2b1f6c358ec786806d63e53195f5489d2633371b
avrhxxx <stelczyk0797@gmail.com>
2026-04-22 11:19:18 +0200

    Create role_ids.py

---
+++ src/ui/definitions/role_ids.py
@@ -0,0 +1,13 @@
+# src/ui/definitions/role_ids.py
+
+class RoleID:
+    """
+    UI access roles (IMPORTANT: NOT admin system roles).
+    This is only for UI rendering layer.
+    """
+
+    R3 = "r3"
+    R4 = "r4"
+    R5 = "r5"
+
+    ADMIN = "admin"
