b77a30fca42e805aa4ae1986cfaf0a2296ef8f36
avrhxxx <stelczyk0797@gmail.com>
2026-04-23 02:41:09 +0200

    Create base.py

---
+++ src/ui/callbacks/base.py
@@ -0,0 +1,9 @@
+from aiogram.filters.callback_data import CallbackData
+
+
+class BaseCB(CallbackData):
+    """
+    Base class for all UI callbacks.
+    Ensures consistent structure across the system.
+    """
+    pass
