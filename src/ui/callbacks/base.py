3f9431c18f98d0086586591de16d0a7f817008ae
avrhxxx <stelczyk0797@gmail.com>
2026-04-23 03:08:51 +0200

    Delete base.py

--- src/ui/callbacks/base.py
+++
@@ -1,9 +0,0 @@
-from aiogram.filters.callback_data import CallbackData
-
-
-class BaseCB(CallbackData):
-    """
-    Base class for all UI callbacks.
-    Ensures consistent structure across the system.
-    """
-    pass
