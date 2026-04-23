681a19ea3301a18834815758efed0bcfbd933425
avrhxxx <stelczyk0797@gmail.com>
2026-04-23 07:15:58 +0200

    Delete src directory

--- src/core/registry.py
+++
@@ -1,5 +0,0 @@
-SCREEN_MAP = {
-    "home_r3": "screens.home.r3_home",
-    "home_r4": "screens.home.r4_home",
-    "home_r5": "screens.home.r5_home",
-}
--- src/ui/definitions/role_ids.py
+++
@@ -1,8 +0,0 @@
-class RoleID:
-    """
-    UI-level role identifiers.
-    """
-
-    R3 = "R3"
-    R4 = "R4"
-    R5 = "R5"
