30bc6799e0a1aa0dbc1b8e3f886417d5296fbdba
avrhxxx <stelczyk0797@gmail.com>
2026-04-24 22:44:40 +0200

    Update dispatcher.py

--- src/factory/dispatcher.py
+++ src/factory/dispatcher.py
@@ -14,6 +14,12 @@ from aiogram_dialog import setup_dialogs
 
 from src.telegram.handlers.start import router as start_router
 
+# dialogs (NEW ARCHITECTURE)
+from src.telegram.dialogs.home.home_dialog import home_dialog
+from src.telegram.dialogs.home.events_dialog import events_dialog
+from src.telegram.dialogs.home.settings_dialog import settings_dialog
+from src.telegram.dialogs.home.help_dialog import help_dialog
+
 logger = logging.getLogger(__name__)
 
 
@@ -32,7 +38,13 @@ def create_dispatcher() -> Dispatcher:
     # =========================
     # DIALOGS
     # =========================
-    logger.info("Setting up aiogram-dialog...")
+    logger.info("Registering dialogs...")
+
+    dp.include_router(home_dialog)
+    dp.include_router(events_dialog)
+    dp.include_router(settings_dialog)
+    dp.include_router(help_dialog)
+
     setup_dialogs(dp)
 
     logger.info("Dispatcher ready")
