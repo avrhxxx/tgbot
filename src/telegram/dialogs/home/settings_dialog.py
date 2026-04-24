b779099ddcd8dd0a63e9d8b06fc3d4440cdc6671
avrhxxx <stelczyk0797@gmail.com>
2026-04-24 22:13:36 +0200

    Update settings_dialog.py

--- src/telegram/dialogs/home/settings_dialog.py
+++ src/telegram/dialogs/home/settings_dialog.py
@@ -8,7 +8,7 @@
 import logging
 from aiogram_dialog import Dialog
 
-from src.telegram.dialogs.home.settings import settings_window
+from src.telegram.windows.home.settings import settings_window
 from src.telegram.dialogs.home.state import SettingsSG
 
 logger = logging.getLogger(__name__)
