a9e4d5c5d21f9ee39145c6660de76382bf7a3a5a
avrhxxx <stelczyk0797@gmail.com>
2026-04-24 22:42:46 +0200

    Update help_dialog.py

--- src/telegram/dialogs/home/help_dialog.py
+++ src/telegram/dialogs/home/help_dialog.py
@@ -9,7 +9,6 @@ import logging
 from aiogram_dialog import Dialog
 
 from src.telegram.windows.home.help import help_window
-from src.telegram.dialogs.home.state import HelpSG
 
 logger = logging.getLogger(__name__)
 
