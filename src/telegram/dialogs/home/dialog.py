b17eaee9e08ce7e01245c62d045cccbb36c2ea74
avrhxxx <stelczyk0797@gmail.com>
2026-04-24 22:05:30 +0200

    Update dialog.py

--- src/telegram/dialogs/home/dialog.py
+++ src/telegram/dialogs/home/dialog.py
@@ -9,20 +9,14 @@ import logging
 from aiogram_dialog import Dialog
 
 from src.telegram.dialogs.home.window import home_window
-from src.telegram.dialogs.home.events import events_window
-from src.telegram.dialogs.home.settings import settings_window
-from src.telegram.dialogs.home.help import help_window
-
+from src.telegram.dialogs.home.state import HomeSG
 
 logger = logging.getLogger(__name__)
 
 
 home_dialog = Dialog(
     home_window,
-    events_window,
-    settings_window,
-    help_window,
 )
 
 
-logger.info("Home dialog initialized with full window stack")
+logger.info("Home dialog initialized (single window mode)")
