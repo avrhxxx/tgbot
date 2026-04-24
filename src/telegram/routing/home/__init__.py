ba01e89f80281a8fdc13d780fed41813265d9874
avrhxxx <stelczyk0797@gmail.com>
2026-04-25 01:17:08 +0200

    Create __init__.py

---
+++ src/telegram/routing/home/__init__.py
@@ -0,0 +1,6 @@
+# =========================================
+# GROUP: telegram.routing.home
+# FILE: __init__.py
+# DESCRIPTION:
+# Home routing package initializer.
+# =========================================
