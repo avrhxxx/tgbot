76bd66bf0bf9b2c0b68f40400ecd78a589158b50
avrhxxx <stelczyk0797@gmail.com>
2026-04-24 22:17:46 +0200

    Create home.py

---
+++ src/telegram/routing/home.py
@@ -0,0 +1,11 @@
+# =========================================
+# GROUP: telegram.routing.home
+# FILE: home.py
+# DESCRIPTION:
+# Routing constants for Home module UI.
+# =========================================
+
+HOME = "home"
+EVENTS = "events"
+SETTINGS = "settings"
+HELP = "help"
