9c0ea8f6a0445d36aa89b193729a847984a40b5c
avrhxxx <stelczyk0797@gmail.com>
2026-04-27 00:50:23 +0200

    Update states.py

--- src/dialogs/panel/states.py
+++ src/dialogs/panel/states.py
@@ -1,10 +1,8 @@
 # src/dialogs/panel/states.py
-# DESCRIPTION:
-# Dialog states for moderator panel.
 
 from aiogram.fsm.state import StatesGroup, State
 
 
 class PanelSG(StatesGroup):
     main = State()
-    broadcast_input = State()
+    broadcast_menu = State()
