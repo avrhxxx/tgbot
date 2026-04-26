5ba44169c3e8e30ef84edba5eeb128bdbb3b55b2
avrhxxx <stelczyk0797@gmail.com>
2026-04-27 00:55:28 +0200

    Update states.py

--- src/dialogs/panel/states.py
+++ src/dialogs/panel/states.py
@@ -6,3 +6,8 @@ from aiogram.fsm.state import StatesGroup, State
 class PanelSG(StatesGroup):
     main = State()
     broadcast_menu = State()
+
+    broadcast_title = State()
+    broadcast_tags = State()
+    broadcast_text = State()
+    broadcast_confirm = State()
