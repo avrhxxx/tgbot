9cdf5c70c234b3b53ebc7544f0b420d8a4b1a652
avrhxxx <stelczyk0797@gmail.com>
2026-04-27 14:38:09 +0200

    Update dispatcher.py

--- src/factory/dispatcher.py
+++ src/factory/dispatcher.py
@@ -10,6 +10,7 @@ from aiogram_dialog import setup_dialogs
 
 from src.handlers.start import router as start_router
 from src.middlewares.config import ConfigMiddleware
+from src.dialogs import register_dialogs
 
 logger = logging.getLogger(__name__)
 
@@ -20,10 +21,13 @@ def create_dispatcher(config) -> Dispatcher:
     # middleware
     dp.update.middleware(ConfigMiddleware(config))
 
-    # routers
+    # handlers
     dp.include_router(start_router)
 
-    # 🔥 CRITICAL
+    # dialogs
+    register_dialogs(dp)
+
+    # dialog engine
     setup_dialogs(dp)
 
     logger.info("Dispatcher ready (dialogs mode)")
