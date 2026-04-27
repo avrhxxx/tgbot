b01225ce4b4081b694fb1e57727c4446022fb8a7
avrhxxx <stelczyk0797@gmail.com>
2026-04-27 10:10:11 +0200

    Update start.py

--- src/handlers/start.py
+++ src/handlers/start.py
@@ -1,24 +1,11 @@
 from aiogram import Router, types
 from aiogram.filters import CommandStart
-from aiogram_dialog import DialogManager, StartMode
 
-from src.dialogs.panel.states import PanelSG
-from src.utils.access import can_use_panel
+from src.flows.announcement_flow import start_flow
 
 router = Router()
 
 
 @router.message(CommandStart())
-async def start_handler(message: types.Message, dialog_manager: DialogManager):
-    user = message.from_user
-    if user is None:
-        return
-
-    if not can_use_panel(user.id):
-        return
-
-    # 🔥 KLUCZ: zawsze reset context
-    await dialog_manager.start(
-        PanelSG.main,
-        mode=StartMode.RESET_STACK
-    )
+async def start_handler(message: types.Message):
+    await message.answer("📣 Ready")
