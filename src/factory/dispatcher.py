e7d3ec721845b3e219521cc8026819ace34d2370
avrhxxx <stelczyk0797@gmail.com>
2026-04-27 04:51:51 +0200

    Update dispatcher.py

--- src/factory/dispatcher.py
+++ src/factory/dispatcher.py
@@ -1,22 +1,25 @@
-# src/factory/dispatcher.py
-# DESCRIPTION:
-# Dispatcher setup with dialog support.
-
-import logging
-
 from aiogram import Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
 
 from src.handlers.start import router as start_router
 from src.dialogs import register_dialogs
 
+from src.middlewares.config import ConfigMiddleware  # 👈 ADD
+
+import logging
+
 logger = logging.getLogger(__name__)
 
 
-def create_dispatcher() -> Dispatcher:
+def create_dispatcher(config) -> Dispatcher:
     logger.info("Starting Dispatcher")
 
     dp = Dispatcher(storage=MemoryStorage())
+
+    # =========================
+    # MIDDLEWARES
+    # =========================
+    dp.update.middleware(ConfigMiddleware(config))  # 👈 FIX
 
     # ROUTERS
     dp.include_router(start_router)
