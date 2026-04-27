3e7cb6420477ae0681fa409bbaea65dc05ff3389
avrhxxx <stelczyk0797@gmail.com>
2026-04-27 10:35:39 +0200

    Update __init__.py

--- src/dialogs/__init__.py
+++ src/dialogs/__init__.py
@@ -2,11 +2,11 @@
 # DESCRIPTION:
 # Registers all dialogs.
 
-from aiogram_dialog import setup_dialogs
+#from aiogram_dialog import setup_dialogs
 
-from src.dialogs.panel.dialog import panel_dialog
+#from src.dialogs.panel.dialog import panel_dialog
 
 
-def register_dialogs(dp):
-    dp.include_router(panel_dialog)
-    setup_dialogs(dp)
+#def register_dialogs(dp):
+    #dp.include_router(panel_dialog)
+    #setup_dialogs(dp)
