294cabfd76520a2063762d8cf9cb1395e2e29385
avrhxxx <stelczyk0797@gmail.com>
2026-04-27 11:11:20 +0200

    Update __init__.py

--- src/handlers/__init__.py
+++ src/handlers/__init__.py
@@ -6,9 +6,9 @@
 # =========================================
 
 from .start import router as start_router
-from .broadcast import router as broadcast_router
+
 
 
 def register_handlers(dp):
     dp.include_router(start_router)
-    dp.include_router(broadcast_router)
+  
