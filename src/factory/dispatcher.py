cba0264a65d54e71405934e93e0f02e4dff7ecae
avrhxxx <stelczyk0797@gmail.com>
2026-04-27 09:38:56 +0200

    Update dispatcher.py

--- src/factory/dispatcher.py
+++ src/factory/dispatcher.py
@@ -8,9 +8,7 @@ from aiogram import Dispatcher
 from aiogram.fsm.storage.memory import MemoryStorage
 
 from src.handlers.start import router as start_router
-from src.dialogs.panel.dialog import router as panel_router  # 🔥 DODANE
 from src.dialogs import register_dialogs
-
 from src.middlewares.config import ConfigMiddleware
 
 import logging
@@ -32,7 +30,6 @@ def create_dispatcher(config) -> Dispatcher:
     # ROUTERS
     # =========================
     dp.include_router(start_router)
-    dp.include_router(panel_router)  # 🔥 KLUCZOWE
 
     # =========================
     # DIALOGS
