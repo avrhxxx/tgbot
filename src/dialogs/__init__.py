75190f6ff27ba13606a6b2f8bd7a33bb17254c75
avrhxxx <stelczyk0797@gmail.com>
2026-04-28 22:59:42 +0200

    Update __init__.py

--- src/dialogs/__init__.py
+++ src/dialogs/__init__.py
@@ -6,8 +6,10 @@ from aiogram import Dispatcher
 
 from src.dialogs.main_menu.dialog import main_menu_dialog
 from src.dialogs.group_message.dialog import group_message_dialog
+from src.dialogs.event_manager.dialog import event_manager_dialog  # 👈 DODAJ
 
 
 def register_dialogs(dp: Dispatcher):
     dp.include_router(main_menu_dialog)
     dp.include_router(group_message_dialog)
+    dp.include_router(event_manager_dialog)  # 👈 DODAJ
